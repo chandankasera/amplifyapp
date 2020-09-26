9865d28aa430863e4642dd4fe25217f1552fa55c
Chandan Kasera <chandankasera@gmail.com>
2020-09-26 12:18:59 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-         <h1> Hello, I love you Babe. Miss you </h1>
+         <h1> Hello, How are you guys </h1>
       </header>
     </div>
   );
